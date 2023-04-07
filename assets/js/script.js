2293e6421c4ebf10a4ea99ae0761bcd433908c78
Pablo Rito <pablorito14@gmail.com>
2023-04-07 11:51:13 -0300

    comentarios en conversion-comentarios.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,26 @@
+// menu oculto = false // menu visible = true
+// por defecto el menu esta oculto
 let menu_open = false 
 
+// buscamos en el DOM el boton del menu
 const btn_menu = document.getElementById('btn-menu');
+
+// escuchamos el evento click
 btn_menu.addEventListener('click', () => {
-
-  console.log(window.innerWidth);
-  if(window.innerWidth <= 768){
-    console.log('cerrar menu');
-  }
-
+  
   if(menu_open === false){
+    // si el menu esta cerrado
+    // lo mostramos poniendo display: grid;
     document.getElementById('menu').style.display = 'grid';
-    
   } else {
+    // si el menu esta abierto
+    // lo ocultamos poniendo display: none
     document.getElementById('menu').style.display = 'none'
   }
 
+  // alternamos el valor de menu.
+  // si estaba oculto (false) lo cambiamos a visible (true)
+  // y si estaba visible (true) lo cambiamos aoculto (false)
   menu_open = !menu_open;
 })
-
-// const openMenu = () => {
-
-  
-// }
 
